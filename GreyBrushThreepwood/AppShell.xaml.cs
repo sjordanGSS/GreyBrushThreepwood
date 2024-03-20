38c2d00537373f37af610587ba56293ceadf0008
Sam Jordan <sjordan@gsspos.co.uk>
2024-03-20 09:21:47 +0000

    Add project files.

---
+++ GreyBrushThreepwood/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GreyBrushThreepwood
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GreyBrushThreepwood/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace GreyBrushThreepwood
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+}
